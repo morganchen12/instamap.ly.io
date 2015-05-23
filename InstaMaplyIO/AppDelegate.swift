b296e723a09b76c4567f6a6b56805989ae4d98ca
Morgan Chen <morganchen12@gmail.com>
2015-05-23 15:35:25 -0700

    initial commit the dream

---
+++ InstaMaplyIO/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  InstaMaplyIO
+//
+//  Created by Morgan Chen on 5/23/15.
+//  Copyright (c) 2015 Morgan Chen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var mapController: MapController?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        if let mapController = self.mapController {
+            mapController.drawMap()
+            UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler({})
+            mapController.startUpdatingLocationInBackground()
+        }
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        if let mapController = self.mapController {
+            mapController.stopUpdatingLocationInBackground()
+        }
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+}
+
---
+++ InstaMaplyIO/MapController.swift
@@ -0,0 +1,99 @@
+//
+//  MapController.swift
+//  InstaMaplyIO
+//
+//  Created by Morgan Chen on 5/23/15.
+//  Copyright (c) 2015 Morgan Chen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import MediaPlayer
+import AVFoundation
+
+final class MapController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager: CLLocationManager = CLLocationManager()
+    var updateTimer: NSTimer? = nil
+    var audioPlayer: AVAudioPlayer? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appDelegateMaybe: AppDelegate? = UIApplication.sharedApplication().delegate as! AppDelegate?
+        if let appDelegate = appDelegateMaybe {
+            appDelegate.mapController = self
+        }
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        
+        var error: NSError? = nil
+        if let resourcePath = NSBundle.mainBundle().resourcePath,
+           let url = NSURL(fileURLWithPath: "\(resourcePath)/blank.mp3"),
+           let audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error) {
+            self.audioPlayer = audioPlayer
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.play()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.updateRegionAnimated(false)
+        self.drawMap()
+    }
+    
+    private func updateRegionAnimated(animated: Bool) {
+        let center = self.locationManager.location.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        self.mapView.setRegion(region, animated: animated)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways {
+            self.mapView.showsUserLocation = true
+        } else {
+            self.locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        self.updateRegionAnimated(true)
+    }
+    
+    func drawMap() {
+        let width = self.mapView.frame.size.width
+        let height = self.mapView.frame.size.height
+        UIGraphicsBeginImageContext(CGSizeMake(width, height))
+        self.mapView.layer.renderInContext(UIGraphicsGetCurrentContext())
+        let mapImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        MPNowPlayingInfoCenter.defaultCenter().nowPlayingInfo = [
+            MPMediaItemPropertyArtwork: MPMediaItemArtwork(image: mapImage),
+            MPMediaItemPropertyArtist: "Don't get lost",
+            MPMediaItemPropertyTitle: "You Are Here",
+        ]
+    }
+    
+    @IBAction func refreshPressed(sender: AnyObject) {
+        self.drawMap()
+    }
+    
+    func updateMapInBackground() {
+        self.updateRegionAnimated(false)
+        self.drawMap()
+    }
+    
+    internal func startUpdatingLocationInBackground() {
+        self.updateTimer = NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(150), target: self, selector: "updateMapInBackground", userInfo: nil, repeats: true)
+    }
+    
+    internal func stopUpdatingLocationInBackground() {
+        if let updateTimer = self.updateTimer {
+            updateTimer.invalidate()
+        }
+        self.updateTimer = nil
+    }
+}
